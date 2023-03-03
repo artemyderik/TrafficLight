776e7b1e264c024b8f0b45de0f1506a6cd25c2f9
ArtemyDerik <derry0808@gmail.com>
2023-03-03 10:57:13 +0700

    MainScreenSetup

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var yellowTrafficLight: UIView!
     @IBOutlet var greenTrafficLight: UIView!
     @IBOutlet var trafficLightButton: UIButton!
+    
     let lightIsOn: CGFloat = 1
     let lightIsOff: CGFloat = 0.3
     
@@ -24,35 +25,18 @@ class ViewController: UIViewController {
         redTrafficLight.layer.cornerRadius = 59
         yellowTrafficLight.layer.cornerRadius = 59
         greenTrafficLight.layer.cornerRadius = 59
-        
-        redTrafficLight.alpha = 0.3
-        yellowTrafficLight.alpha = 0.3
-        greenTrafficLight.alpha = 0.3
-        
         trafficLightButton.layer.cornerRadius = 8
+        
+        redTrafficLight.alpha = lightIsOff
+        yellowTrafficLight.alpha = lightIsOff
+        greenTrafficLight.alpha = lightIsOff
+        
+       
     }
     
     
     @IBAction func trafficLightButtonDidTapped() {
         trafficLightButton.setTitle("Next", for: .normal)
-        
-        /*   if redTrafficLight.isOpaque {
-         redTrafficLight.alpha = 1
-         redTrafficLight.isOpaque.toggle()
-         } else if yellowTrafficLight.isOpaque {
-         yellowTrafficLight.alpha = 1
-         yellowTrafficLight.isOpaque.toggle()
-         redTrafficLight.alpha = 0.3
-         } else if greenTrafficLight.isOpaque {
-         greenTrafficLight.alpha = 1
-         greenTrafficLight.isOpaque.toggle()
-         yellowTrafficLight.alpha = 0.3
-         } else if !greenTrafficLight.isOpaque {
-         greenTrafficLight.alpha = 0.3
-         redTrafficLight.alpha = 1
-         }
-         } */
-        
         if redTrafficLight.alpha == yellowTrafficLight.alpha {
             redTrafficLight.alpha = lightIsOn
             greenTrafficLight.alpha = lightIsOff
